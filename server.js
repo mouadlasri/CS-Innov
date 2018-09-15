d2003c9ecb262b638acc7e8af35b283db4cf9ecb
Mello <lasri.mouad98@gmail.com>
2018-09-15 19:14:11 +0100

    Added backend

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ const app = express();
 // Bodyparser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+// enable cors
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
+// server static files from the react app
 app.use(express.static(__dirname, 'client/build'));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
