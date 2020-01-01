94a700906bd8aa5d6ce000d7255501299676d142
Mouad <lasri.mouad98@gmail.com>
2020-01-01 14:51:40 +0100

    Whole project migrated

--- client/src/components/Aboutpage/Aboutpage.js
+++ client/src/components/Aboutpage/Aboutpage.js
@@ -9,18 +9,19 @@ const Aboutpage = () => {
             <div className="Toolbar-color">
                 <Toolbar />
             </div>
-            <div className="row no-gutters">
                 <h1 className="about-title">Who are We?</h1>
+
+            <div className="row no-gutters">
                 <div className="col-xs-12 col-sm-12 col-md-10 mr-auto ml-auto">
                     <p style={{ marginBottom: "50px" }}>Computer Science for Innovation club is a students' organization that strives to promote Technology & Innovation 
                         within Al Akhawayn Community through various events, workshops and fairs. </p>
                     <div className="row">
-                        <div className="col-xs-12 col-sm-12 col-md-5 about-image-1">
-                            
+                        <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+                            <img src={require('./images/web-workshop.jpg')} style={{ width: "500px"}} />
                         </div>
-                        <div className="col-xs-0 col-sm-2 col-md-2"></div>
-                        <div className="col-xs-12 col-sm-12 col-md-5 about-image-2">
-        
+                        <div className="col-xs-0 col-sm-2 col-md-2 col-lg-2"></div>
+                        <div className="col-xs-12 col-sm-12 col-md-5">
+                            <img src={require('./images/android-workshop.jpg')} style={{ width: "500px"}} />
                         </div>
                     </div>
                 </div>
